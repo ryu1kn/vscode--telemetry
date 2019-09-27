893d167871d407a6bd4190737188b3a6eb034a60
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-27 20:51:47 +1000

    Deploy to production environment

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-echo '===> Starting test'
-
 curl -f -X POST $API_DOMAIN/telemetry-data -H 'Content-Type: application/json' -d '{"a": 2}'
 
-echo '===> Passed!'
+echo Passed!
--- travis-build.sh
+++ travis-build.sh
@@ -2,5 +2,18 @@
 
 set -euo pipefail
 
+message() {
+    echo -e "\n===> $1"
+}
+
+message "Deploying to preprod"
 ENV_NAME=preprod infra/deploy.sh
+
+message "Testing against preprod"
 API_DOMAIN=https://vscode-telemetry-preprod.ryuichi.io ./test.sh
+
+message "Deploying to prod"
+ENV_NAME=prod infra/deploy.sh
+
+message "Testing against prod"
+API_DOMAIN=https://vscode-telemetry.ryuichi.io ./test.sh
