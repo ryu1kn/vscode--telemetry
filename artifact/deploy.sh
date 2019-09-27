a1fe6336a3c33f58a193d3c7561b281efa046425
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-28 01:38:06 +1000

    Tiny lambda doesn't need to be pre-uploaded

--- artifact/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-current_dir="$(dirname "$0")"
-
-aws cloudformation deploy --stack-name "vscode-telemetry-artifact" \
-    --template-file "$current_dir/template.yaml" \
-    --no-fail-on-empty-changeset
