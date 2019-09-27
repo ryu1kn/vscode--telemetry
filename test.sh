27a2e2510cdb2d9f2dfeefb0e16ad20eb2b2b4e0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-27 18:41:46 +1000

    Specify a path

--- test.sh
+++ test.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
+echo '===> Starting test'
+
 curl -f -X POST $API_DOMAIN/telemetry-data -H 'Content-Type: application/json' -d '{"a": 2}'
+
+echo '===> Passed!'
--- travis-build.sh
+++ travis-build.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 ENV_NAME=preprod infra/deploy.sh
-API_DOMAIN=https://vscode-telemetry-preprod.ryuichi.io test.sh
+API_DOMAIN=https://vscode-telemetry-preprod.ryuichi.io ./test.sh
